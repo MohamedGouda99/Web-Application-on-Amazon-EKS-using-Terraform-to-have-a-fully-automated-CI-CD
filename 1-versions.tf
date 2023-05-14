e2b82722a0bfcdc7922c1e6c7ad2ca3e904e41be
MohamedGouda99 <ma1582830@gmail.com>
2023-05-15 00:37:48 +0300

    final commit

--- 1-versions.tf
+++ 1-versions.tf
@@ -1,15 +1,15 @@
 #Terraform Block
 
-terraform {
-  required_version = "~> 1.3.7"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+# terraform {
+#   required_version = "~> 1.3.7"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
 
-}
+# }
 
 #Provider Block
 provider "aws" {
