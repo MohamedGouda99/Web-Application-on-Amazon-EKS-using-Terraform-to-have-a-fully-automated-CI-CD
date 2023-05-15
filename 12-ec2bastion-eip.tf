adf408cbe6949e9af8453f976ddd37a55ff92b5f
MohamedGouda99 <ma1582830@gmail.com>
2023-05-15 05:47:18 +0300

    final push

--- 12-ec2bastion-eip.tf
+++ 12-ec2bastion-eip.tf
@@ -1,5 +1,5 @@
 # Create Elastic IP for Bastion Host
-# Resource - depends_on Meta-Argument
+
 resource "aws_eip" "bastion_eip" {
   depends_on = [module.ec2_public, module.vpc]
   instance   = module.ec2_public.id
--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,4 @@
+# Resource ECR Caleed ngnix-app for storing images
 resource "aws_ecr_repository" "foo1" {
   name                 = "nginx-app"
   image_tag_mutability = "MUTABLE"
